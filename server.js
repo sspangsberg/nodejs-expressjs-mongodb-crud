c2ccceb3803c9b9340463d75b9af2d2a92189967
sms_easv <smsj@easv.dk>
2020-07-31 22:21:00 +0200

    Mongo DB setup

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 //initialize dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+let connectionString = 'mongodb+srv://mongo_user:Mygind4077@cluster0-usxmc.mongodb.net/star-wars-quotes?retryWrites=true&w=majority';
+
+//initialize ExpressJS app
 const app = express();
 
-//make sure to setup body-parser before routes!
-app.use(bodyParser.urlencoded({extended : true}));
+//connect to mongo db
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
 
+        console.log('Connected to Mongo DB')
 
-app.listen(80, function() {
-    console.log('listening on 80');
-})
+        //make sure to setup body-parser before routes!
+        app.use(bodyParser.urlencoded({extended : true}));
+        
+        const db = client.db('star-wars-quotes');
+        
+        app.get('/', function(req, res) {
+            console.log(__dirname);
+            res.sendFile(__dirname + '/views/index.html');
+            //__dirname is current directory your in.
+        })
+        
+        app.post('/quotes', (req, res) => {
+            console.log(req.body.name);
+            console.log(req.body.quote);
+        })
 
-app.get('/', function(req, res) {
-    console.log(__dirname);
-    res.sendFile(__dirname + '/views/index.html');
-    //__dirname is current directory your in.
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.quote);
-})
+        app.listen(80, () => { console.log('listening on 80') })
+    })
+    .catch(error => console.log(error))
+ 
